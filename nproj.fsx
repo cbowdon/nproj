c1b612aa6362e9515f814837faf84482a038c8be
Chris Bowdon <c.bowdon@bath.edu>
2015-05-08 22:17:04 +0000

    Implement most of XML reading (badly)

--- nproj.fsx
+++ nproj.fsx
@@ -10,9 +10,6 @@ open System.Xml
 open System.Xml.Linq
 
 [<Literal>]
-let schema = "http://schemas.microsoft.com/developer/msbuild/2003"
-
-[<Literal>]
 let fsprojFile = "Sample/Sample.fsproj"
 
 [<Literal>]
@@ -22,9 +19,9 @@ type OutputType = Exe | Library
 
 type FileType = Source | Reference | Data | Project
 
-type Library = { Include: string; Private: bool option }
+type Library = { Include: Uri; Private: bool option }
 
-type SourceFile = SourceFile of Uri | DataFile of Uri
+type Include = SourceFile of Uri | DataFile of Uri
 
 type ItemType = Reference | File | ProjectReference
 
@@ -45,17 +42,21 @@ type Configuration = {
 }
 
 type Project = {
-    Name: string
     File: Uri
-    References: Library list
-    ProjectReferences: Project list
-    Files: SourceFile list
     Properties: Properties
     Configurations: Configuration list
+    References: Library list
+    Includes: Include list
+    ProjectReferences: Project list
 }
 
 // Serialization
 module Xml =
+
+    let schema = "http://schemas.microsoft.com/developer/msbuild/2003"
+
+    let xname (name: string): XName = XName.Get(name, schema)
+
     module Write =
         let project (proj: Project): XDocument = failwith "TODO"
         let configuration (conf: Configuration): XElement = failwith "TODO"
@@ -63,16 +64,64 @@ module Xml =
         let import (imp: Import): XElement = failwith "TODO"
         let reference (lib: Library): XElement = failwith "TODO"
         let projectReference (lib: Library): XElement = failwith "TODO"
-        let sourceFile (file: SourceFile): XElement = failwith "TODO"
+        let sourceFile (file: Include): XElement = failwith "TODO"
         let toDisk (file: Uri) (xdoc: XDocument): unit = failwith "TODO"
+
     module Read =
-        let project (proj: XDocument): Project = failwith "TODO"
+
         let configuration (conf: XElement): Configuration = failwith "TODO"
-        let properties (prop: XElement): Properties = failwith "TODO"
         let import (imp: XElement): Import = failwith "TODO"
-        let reference (lib: XElement): Library = failwith "TODO"
-        let projectReference (lib: XElement): Library = failwith "TODO"
-        let sourceFile (file: XElement): SourceFile = failwith "TODO"
+
+        let inclusion (file: XElement): Uri =
+            let inc = file.Attribute(xname "Include")
+            Uri(inc.Value)
+
+        let reference (lib: XElement): Library =
+            let privs = lib.Elements(xname "Private")
+            let priv =
+                if Seq.isEmpty privs
+                then None
+                else privs.First().Value |> bool.Parse |> Some
+            { Include = inclusion lib; Private = priv }
+
+        let projectReference (lib: XElement): Project = failwith "TODO"
+
+        let sourceFile (file: XElement): Include =
+            let inc = file.Attribute(xname "Include")
+            let uri = Uri(inc.Value)
+            match file.Name.ToString() with
+            | "Compile" -> SourceFile uri
+            | "None" -> DataFile uri
+            | _ -> failwith "Is not a source file or data file"
+
+        let properties (prop: XElement): Properties =
+            let name = prop.Element(xname "Name").Value
+            let guid = prop.Element(xname "ProjectGuid").Value |> Guid.Parse
+            let output =
+                match prop.Element(xname "OutputType").Value with
+                | "Exe" -> Exe
+                | "Library" -> Library
+                | _ -> failwith "Invalid output type in proj file"
+            { Name = name; Guid = guid; OutputType = output }
+
+        let project (file: Uri) (proj: XDocument): Project =
+            let propertyGroups = proj.Descendants(xname "PropertyGroup")
+            let isConf (xel: XElement) = xel.Elements(xname "OutputPath").Any()
+            let props = propertyGroups.First(fun p -> p |> isConf |> not) |> properties
+            let confs = propertyGroups.SkipWhile(fun p -> p |> isConf) |> Seq.map configuration
+
+            let itemGroups = proj.Descendants(xname "ItemGroup")
+            let refs = itemGroups.Descendants(xname "Reference") |> Seq.map reference
+            let compiles = itemGroups.Descendants(xname "Compile") |>  Seq.map sourceFile
+            let nones = itemGroups.Descendants(xname "None") |>  Seq.map sourceFile
+            {
+                File = file
+                Properties = props
+                Configurations = [] // TODO
+                References = List.ofSeq refs
+                Includes = Seq.append compiles nones |> List.ofSeq
+                ProjectReferences = [] // TODO
+            }
 
 // Parsing user commands
 module Read =
@@ -93,6 +142,7 @@ module Read =
         match input.ToLowerInvariant() with
         | "exe" -> Some Exe
         | "lib" -> Some Library
+        | "library" -> Some Library
         | _ -> None
 
     let filename (input: string): ItemType option =
@@ -107,7 +157,7 @@ module Read =
     let project (input: string): Project option =
         match projectFile input with
         | None -> None
-        | Some p -> p.AbsolutePath |> XDocument.Load |> Xml.Read.project |> Some
+        | Some p -> p.AbsolutePath |> XDocument.Load |> Xml.Read.project p |> Some
 
 
 // Main operations
@@ -144,4 +194,4 @@ let main (args: string list): int =
     | "mv"::rest -> failwith "TODO move"
     | _ -> failwith "TODO no args specified - usage statement"
 
-fsi.CommandLineArgs |> Seq.skip 1 |> List.ofSeq |> main
+//fsi.CommandLineArgs |> Seq.skip 1 |> List.ofSeq |> main
