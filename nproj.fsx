8a6c232383f1fb7cc86c912b7c6175f0257bd142
Chris Bowdon <c.bowdon@bath.edu>
2015-05-08 22:36:24 +0000

    Fix bugs in Read.project

--- nproj.fsx
+++ nproj.fsx
@@ -10,16 +10,16 @@ open System.Xml
 open System.Xml.Linq
 
 [<Literal>]
-let fsprojFile = "Sample/Sample.fsproj"
+let fsprojFile = "./Sample/Sample.fsproj"
 
 [<Literal>]
-let assemblyInfoFile = "Sample/AssemblyInfo.fs"
+let assemblyInfoFile = "./Sample/AssemblyInfo.fs"
 
 type OutputType = Exe | Library
 
 type FileType = Source | Reference | Data | Project
 
-type Library = { Include: Uri; Private: bool option }
+type Library = { Include: string; Private: bool option }
 
 type Include = SourceFile of Uri | DataFile of Uri
 
@@ -56,6 +56,7 @@ module Xml =
     let schema = "http://schemas.microsoft.com/developer/msbuild/2003"
 
     let xname (name: string): XName = XName.Get(name, schema)
+    let xnons (name: string): XName = XName.Get name // XName with no namespace
 
     module Write =
         let project (proj: Project): XDocument = failwith "TODO"
@@ -72,22 +73,19 @@ module Xml =
         let configuration (conf: XElement): Configuration = failwith "TODO"
         let import (imp: XElement): Import = failwith "TODO"
 
-        let inclusion (file: XElement): Uri =
-            let inc = file.Attribute(xname "Include")
-            Uri(inc.Value)
-
         let reference (lib: XElement): Library =
             let privs = lib.Elements(xname "Private")
             let priv =
                 if Seq.isEmpty privs
                 then None
                 else privs.First().Value |> bool.Parse |> Some
-            { Include = inclusion lib; Private = priv }
+            let lib = lib.Attribute(xnons "Include").Value
+            { Include = lib; Private = priv }
 
         let projectReference (lib: XElement): Project = failwith "TODO"
 
-        let sourceFile (file: XElement): Include =
-            let inc = file.Attribute(xname "Include")
+        let inclusion (file: XElement): Include =
+            let inc = file.Attribute(xnons "Include")
             let uri = Uri(inc.Value)
             match file.Name.ToString() with
             | "Compile" -> SourceFile uri
@@ -112,8 +110,8 @@ module Xml =
 
             let itemGroups = proj.Descendants(xname "ItemGroup")
             let refs = itemGroups.Descendants(xname "Reference") |> Seq.map reference
-            let compiles = itemGroups.Descendants(xname "Compile") |>  Seq.map sourceFile
-            let nones = itemGroups.Descendants(xname "None") |>  Seq.map sourceFile
+            let compiles = itemGroups.Descendants(xname "Compile") |>  Seq.map inclusion
+            let nones = itemGroups.Descendants(xname "None") |>  Seq.map inclusion
             {
                 File = file
                 Properties = props
@@ -134,9 +132,10 @@ module Read =
                 Uri(uri, projFilename) |> Some
             else
                 None
+        let fullPath = Path.GetFullPath(input)
         match Path.GetExtension(input).ToLowerInvariant() with
-        | "fsproj" -> Uri(input) |> Some
-        | _ -> try projFileForDir input with | _ -> None
+        | ".fsproj" -> Uri(fullPath) |> Some
+        | _ -> try projFileForDir fullPath with | _ -> None
 
     let outputType (input: string): OutputType option =
         match input.ToLowerInvariant() with
@@ -147,9 +146,9 @@ module Read =
 
     let filename (input: string): ItemType option =
         match Path.GetExtension(input).ToLowerInvariant() with
-        | "fs" -> Some File
-        | "fsproj" -> Some ProjectReference
-        | "dll" -> Some Reference
+        | ".fs" -> Some File
+        | ".fsproj" -> Some ProjectReference
+        | ".dll" -> Some Reference
         | _ -> Some File
 
     let file (input: string): Uri option = try Uri(input) |> Some with | _ -> None
@@ -195,3 +194,5 @@ let main (args: string list): int =
     | _ -> failwith "TODO no args specified - usage statement"
 
 //fsi.CommandLineArgs |> Seq.skip 1 |> List.ofSeq |> main
+
+Read.project fsprojFile
