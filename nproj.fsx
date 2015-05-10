544b5686b2c437851e8d8ccc4f927de9f3b72d15
Chris Bowdon <c.bowdon@bath.edu>
2015-05-10 21:27:36 +0000

    Implement command parsing (badly)

--- nproj.fsx
+++ nproj.fsx
@@ -50,6 +50,10 @@ type Project = {
     ProjectReferences: Project list
 }
 
+type Command =
+    Init of Uri * OutputType
+    | Add of Project * Uri
+
 // Serialization
 module Xml =
 
@@ -58,7 +62,7 @@ module Xml =
     let xname (name: string): XName = XName.Get(name, schema)
     let xnons (name: string): XName = XName.Get name // XName with no namespace
 
-    module Write =
+    module Marshal =
         let configuration (conf: Configuration): XElement = failwith "TODO"
 
         let properties (prop: Properties): XElement =
@@ -98,7 +102,8 @@ module Xml =
                     ]))
 
     module IO =
-        let toDisk (file: Uri) (xdoc: XDocument): unit = failwith "TODO"
+        let writeToDisk (project: Project): unit = failwith "TODO"
+
         let settings: XmlWriterSettings =
             let settings = XmlWriterSettings()
             settings.Encoding <- Encoding.UTF8
@@ -109,7 +114,7 @@ module Xml =
             settings.NewLineChars <- Environment.NewLine
             settings
 
-    module Read =
+    module Unmarshal =
 
         let configuration (conf: XElement): Configuration = failwith "TODO"
         let import (imp: XElement): Import = failwith "TODO"
@@ -162,21 +167,38 @@ module Xml =
                 ProjectReferences = [] // TODO
             }
 
+module Util =
+
+    let assemblyInfo (project: Project): string = failwith "TODO"
+
+    let printUsage (): unit =
+        File.ReadAllLines("README.org") // temporary solution
+        |> Seq.iter Console.WriteLine
+
+    let item (input: string): ItemType option =
+        if not(File.Exists(input))
+        then None
+        else
+            match Path.GetExtension(input).ToLowerInvariant() with
+            | ".fs" -> Some File
+            | ".fsproj" -> Some ProjectReference
+            | ".dll" -> Some Reference
+            | _ -> Some File
+
+    let relatedProjectFile (uri: Uri): Uri =
+        let ext = Path.GetExtension(uri.AbsolutePath)
+        match ext.ToLowerInvariant() with
+        | ".fsproj" -> uri
+        | _ -> let projFilename = uri.Segments.Last() |> sprintf "%s.fsproj"
+               Uri(uri, projFilename)
+
 // Parsing user commands
-module Read =
-    let projectFile (input: string): Uri option =
-        let projFileForDir dirPath =
-            if Directory.Exists(dirPath)
-            then
-                let uri = Uri(dirPath)
-                let projFilename = uri.Segments.Last() |> sprintf "%s.fsproj"
-                Uri(uri, projFilename) |> Some
-            else
-                None
-        let fullPath = Path.GetFullPath(input)
-        match Path.GetExtension(input).ToLowerInvariant() with
-        | ".fsproj" -> Uri(fullPath) |> Some
-        | _ -> try projFileForDir fullPath with | _ -> None
+module Parse =
+    let location (input: string): Uri option =
+        try
+            input |> Path.GetFullPath |> (fun u -> Uri(u)) |> Some
+        with
+            | _ -> None
 
     let outputType (input: string): OutputType option =
         match input.ToLowerInvariant() with
@@ -185,49 +207,76 @@ module Read =
         | "library" -> Some Library
         | _ -> None
 
-    let filename (input: string): ItemType option =
-        match Path.GetExtension(input).ToLowerInvariant() with
-        | ".fs" -> Some File
-        | ".fsproj" -> Some ProjectReference
-        | ".dll" -> Some Reference
-        | _ -> Some File
-
-    let file (input: string): Uri option = try Uri(Path.GetFullPath(input)) |> Some with | _ -> None
-
     let project (input: string): Project option =
-        match projectFile input with
+        match location input with
         | None -> None
-        | Some p -> p.AbsolutePath |> XDocument.Load |> Xml.Read.project p |> Some
+        | Some p -> p.AbsolutePath |> XDocument.Load |> Xml.Unmarshal.project p |> Some
+
+    // yo F# this is where a built-in Maybe computation expression would be helpful...
+    // I don't want to roll my own or include a lib just for this!
+    let bind (opt: 'a option) (f: 'a -> 'b option): 'b option =
+        match opt with
+        | None -> None
+        | Some x -> f x
+
+    module Command =
+        let init (uri: string) (output: string): Command option =
+            let uriOpt = location uri
+            let outputOpt = outputType output
+            bind uriOpt (fun uri ->
+            bind outputOpt (fun output ->
+            Init(uri, output) |> Some))
+
+        let add (uri: string) (proj: string): Command option =
+            let uriOpt = location uri
+            let projOpt = project proj
+            bind uriOpt (fun uri ->
+            bind projOpt (fun proj ->
+            Add(proj, uri) |> Some))
+
+    let command (args: string seq): Command option =
+        match List.ofSeq args with
+        // TODO support optional and default arguments
+        | ["init"; uri; "--outputType"; outputType ] -> Command.init uri outputType
+        // TODO support multiple filenames
+        | ["add"; fileUri; "--project"; projUri ] -> Command.add fileUri projUri
+        // TODO support aliases
+        | ["remove"; fileUri; "--project"; projUri ] -> failwith "TODO"
+        | ["move"; sourceUri; targetUri; "--project"; projUri ] -> failwith "TODO"
+        | _ -> None
 
 
 // Main operations
 module NProj =
-    let init (directory: Uri) (outputType: OutputType): Project = failwith "TODO"
-    let add (project: Project) (filename: Uri): Project = failwith "TODO"
+    let init (directory: Uri) (outputType: OutputType): Project =
+        {
+            File = Util.relatedProjectFile directory
+            Properties = { Name = ""; Guid = Guid.NewGuid(); OutputType = outputType }
+            Configurations = [] // TODO defaults
+            References = [] // TODO defaults
+            Includes = [] // TODO defaults
+            ProjectReferences = [] // TODO defaults
+        }
+
+    let add (project: Project) (file: Uri): Project = failwith "TODO"
     let remove (project: Project) (filename: Uri): Project = failwith "TODO"
     let move (project: Project) (source: Uri) (target: Uri): Project = failwith "TODO"
 
-// Utility funcs
-let assemblyInfo (project: Project): string = failwith "TODO"
+let main (args: string seq): int =
+    match Parse.command args with
+    | None -> Util.printUsage(); 1
+    | Some command ->
+        let result =
+            match command with
+            | Init (directory, outputType) -> NProj.init directory outputType
+            | Add (project, file) -> NProj.add project file
+        Xml.IO.writeToDisk result
+        0
 
-let printUsage (): unit =
-    File.ReadAllLines("README.org") // temporary solution
-    |> Seq.iter Console.WriteLine
+//fsi.CommandLineArgs |> Seq.skip 1 |> main
 
-let main (args: string list): int =
-    match args with
-    | "init"::rest -> failwith "TODO init"
-    | "add"::rest -> failwith "TODO add"
-    | "remove"::rest -> failwith "TODO remove"
-    | "rm"::rest -> failwith "TODO remove"
-    | "move"::rest -> failwith "TODO move"
-    | "mv"::rest -> failwith "TODO move"
-    | _ -> failwith "TODO no args specified - usage statement"
-
-//fsi.CommandLineArgs |> Seq.skip 1 |> List.ofSeq |> main
-
-let proj = Read.project fsprojFile
+let proj = Parse.project fsprojFile
 let xml =
     match proj with
-    | Some p -> Xml.Write.project p
+    | Some p -> Xml.Marshal.project p
     | None -> failwith "None!"
