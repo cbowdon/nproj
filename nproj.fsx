69bbc9a5dc3126036c22c2ee0409a10d09fbf4e8
Chris Bowdon <c.bowdon@bath.edu>
2015-06-07 23:08:06 +0000

    Skeleton program

--- nproj.fsx
+++ nproj.fsx
@@ -10,16 +10,65 @@
 #r "../msbuild/bin/Unix/Debug-MONO/Microsoft.Build.Tasks.Core.dll"
 #r "../msbuild/bin/Unix/Debug-MONO/Microsoft.Build.Utilities.Core.dll"
 
-open Microsoft.Build.Evaluation
-open Microsoft.Build.Framework
+module Project =
+    open Microsoft.Build.Evaluation
+    open Microsoft.Build.Framework
 
-// Testy test
-let sample = "Sample/Sample.fsproj"
+    let init (args: Init): int = failwith "undefined"
+    let add (args: Add): int = failwith "undefined"
+    let remove (args: Remove): int = failwith "undefined"
+    let move (args: Move): int = failwith "undefined"
 
-let proj = Project(sample)
+    (*
+    // Testy test
+    let sample = "Sample/Sample.fsproj"
 
-proj.AddItem("Compile", "Test.fs")
+    let proj = Project(sample)
 
-proj.Save()
+    proj.AddItem("Compile", "Test.fs")
 
-// TODO - rest of the damn project
+    proj.Save()
+    *)
+
+open System
+
+type ProjectPath = Directory | ProjectFile
+type Language = CSharp | FSharp
+type OutputType = Library | Exe
+
+type Init = {
+    Path: ProjectPath
+    Lang: Language
+    Type: OutputType }
+
+type Add = {
+    Paths: Uri seq
+    Project: ProjectPath }
+
+type Remove = {
+    Paths: Uri seq
+    Project: ProjectPath }
+
+type Move = {
+    From: Uri
+    To: Uri
+    Project: ProjectPath }
+
+module Parsers =
+
+    let init (args: string seq): Init = failwith "undefined"
+    let add (args: string seq): Add = failwith "undefined"
+    let remove (args: string seq): Remove = failwith "undefined"
+    let move (args: string seq): Move = failwith "undefined"
+
+let help (): int = failwith "undefined"
+
+let main (args: string[]): int =
+    match List.ofSeq args with
+    | "init"::rest -> rest |> Parsers.init |> Project.init
+    | "add"::rest -> rest |> Parsers.add |> Project.add
+    | "remove"::rest -> rest |> Parsers.remove |> Project.remove
+    | "rm"::rest -> rest |> Parsers.remove |> Project.remove
+    | "move"::rest -> rest |> Parsers.move |> Project.move
+    | "mv"::rest -> rest |> Parsers.move |> Project.move
+    | _ -> help()
