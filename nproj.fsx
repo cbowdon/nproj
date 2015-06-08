64561ff13601ee2b71c7d82a4d8cc75f0260a49d
Chris Bowdon <c.bowdon@bath.edu>
2015-06-08 19:56:37 +0000

    Implement parsing for init command

--- nproj.fsx
+++ nproj.fsx
@@ -5,36 +5,25 @@
 //   git checkout xplat
 //   ./build.pl
 // Must reference all the custom assemblies or will fall back to GAC version.
+// TODO make msbuild a submodule
 #r "../msbuild/bin/Unix/Debug-MONO/Microsoft.Build.dll"
 #r "../msbuild/bin/Unix/Debug-MONO/Microsoft.Build.Framework.dll"
 #r "../msbuild/bin/Unix/Debug-MONO/Microsoft.Build.Tasks.Core.dll"
 #r "../msbuild/bin/Unix/Debug-MONO/Microsoft.Build.Utilities.Core.dll"
 
-module Project =
-    open Microsoft.Build.Evaluation
-    open Microsoft.Build.Framework
-
-    let init (args: Init): int = failwith "undefined"
-    let add (args: Add): int = failwith "undefined"
-    let remove (args: Remove): int = failwith "undefined"
-    let move (args: Move): int = failwith "undefined"
-
-    (*
-    // Testy test
-    let sample = "Sample/Sample.fsproj"
-
-    let proj = Project(sample)
-
-    proj.AddItem("Compile", "Test.fs")
-
-    proj.Save()
-    *)
-
 open System
 
-type ProjectPath = Directory | ProjectFile
-type Language = CSharp | FSharp
-type OutputType = Library | Exe
+type ProjectPath =
+    | Folder of string
+    | ProjectFile of string
+
+type Language =
+    | FSharp
+    | CSharp
+
+type OutputType =
+    | Library
+    | Exe
 
 type Init = {
     Path: ProjectPath
@@ -54,9 +43,65 @@ type Move = {
     To: Uri
     Project: ProjectPath }
 
+module Read =
+    open System.IO
+
+    let projectPath (path: string): ProjectPath =
+        let path'= Path.GetFullPath(path)
+        if Directory.Exists(path')
+        then Folder path'
+        else ProjectFile path' // TODO validate that projectFile is a *proj
+
+    let language (lang: string): Language =
+        match lang.ToLowerInvariant() with
+        | "fsharp" -> FSharp
+        | "f#" -> FSharp
+        | "csharp" -> CSharp
+        | "c#" -> CSharp
+        | _ -> lang |> sprintf "Language not recognised: %s" |> failwith
+
+    let outputType (ot: string): OutputType =
+        match ot.ToLowerInvariant() with
+        | "library" -> Library
+        | "lib" -> Library
+        | "executable" -> Exe
+        | "exe" -> Exe
+        | _ -> ot |> sprintf "Output type not recognised: %s" |> failwith
+
+module Project =
+  open Microsoft.Build.Evaluation
+  open Microsoft.Build.Framework
+
+  let init (args: Init): int = failwith "undefined"
+  let add (args: Add): int = failwith "undefined"
+  let remove (args: Remove): int = failwith "undefined"
+  let move (args: Move): int = failwith "undefined"
+
+  (*
+  // Testy test
+  let sample = "Sample/Sample.fsproj"
+
+  let proj = Project(sample)
+
+  proj.AddItem("Compile", "Test.fs")
+
+  proj.Save()
+    *)
+
 module Parsers =
 
-    let init (args: string seq): Init = failwith "undefined"
+    let init (args: string seq): Init =
+        let rec init' acc args =
+            match args with
+            | "--lang"::lang::rest -> init' { acc with Lang = Read.language lang } rest
+            | "--type"::ot::rest -> init' { acc with Type = Read.outputType ot } rest
+            | dir::rest -> init' { acc with Path = Read.projectPath dir } rest
+        let acc' = {
+            Path = Folder "."
+            Lang = FSharp
+            Type = Library }
+        args |> List.ofSeq |> init' acc'
+
     let add (args: string seq): Add = failwith "undefined"
     let remove (args: string seq): Remove = failwith "undefined"
     let move (args: string seq): Move = failwith "undefined"
