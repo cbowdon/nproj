d8322eb7cac50c5dbf657366316777c5648f8920
Chris Bowdon <c.bowdon@bath.edu>
2015-07-26 06:06:21 +0000

    Implement test property data for Add.parse

--- NProj.Test/Add.Test.fs
+++ NProj.Test/Add.Test.fs
@@ -1,45 +1,29 @@
 namespace NProj.Test
 
-module Add
+module Add =
     open System
     open Xunit
     open NProj
     open NProj.Common
 
-    let parseAdd = [ [| String.Empty |] ]
- (*
-    Tests performed in REPL
+    let objectArrayOf (x: string list, y: Add.AddCommand): Object[] = [| x; y |]
 
-let addTest0 = Add.parse [ "one.fs" ]
-let addTest1 = Add.parse [ "one.fs"; "two.fs" ]
-let addTest2 = Add.parse [ "one.fs"; "two.fs"; "--project"; "." ]
-let addTest3 = Add.parse [ "one.fs"; "two.fs"; "--project"; "NProj.fsproj" ]
-
-val addTest0 : Add.AddCommand =
-  {SourceFiles = [{Location = file:///home/vagrant/Working/NProj/one.fs;
-                   Exists = false;}];
-   ProjectFile = Directory file:///home/vagrant/Working/NProj;}
-val addTest1 : Add.AddCommand =
-  {SourceFiles =
-    [{Location = file:///home/vagrant/Working/NProj/two.fs;
-      Exists = false;}; {Location = file:///home/vagrant/Working/NProj/one.fs;
-                         Exists = false;}];
-   ProjectFile = Directory file:///home/vagrant/Working/NProj;}
-val addTest2 : Add.AddCommand =
-  {SourceFiles =
-    [{Location = file:///home/vagrant/Working/NProj/two.fs;
-      Exists = false;}; {Location = file:///home/vagrant/Working/NProj/one.fs;
-                         Exists = false;}];
-   ProjectFile = Directory file:///home/vagrant/Working/NProj;}
-val addTest3 : Add.AddCommand =
-  {SourceFiles =
-    [{Location = file:///home/vagrant/Working/NProj/two.fs;
-      Exists = false;}; {Location = file:///home/vagrant/Working/NProj/one.fs;
-                         Exists = false;}];
-   ProjectFile = File file:///home/vagrant/Working/NProj/NProj.fsproj;}
-
-*)
+    let parseAdd =
+      Seq.map objectArrayOf [
+          ( [ "one.fs"], { SourceFiles = [ "/home/vagrant/Working/NProj/one.fs" |> uri |> Compile ];
+                           ProjectFile = "/home/vagrant/Working/NProj" |> uri |> Directory });
+          ( [ "one.fs"; "two.fs" ], { SourceFiles = [ "/home/vagrant/Working/NProj/one.fs" |> uri |> Compile; "/home/vagrant/Working/NProj/two.fs" |> uri |> Compile ];
+                                      ProjectFile = "/home/vagrant/Working/NProj" |> uri |> Directory });
+          ( [ "one.fs"; "two.fs"; "--project"; "." ], { SourceFiles = [ "/home/vagrant/Working/NProj/one.fs" |> uri |> Compile; "/home/vagrant/Working/NProj/two.fs" |> uri |> Compile ];
+                                                        ProjectFile = "/home/vagrant/Working/NProj" |> uri |> Directory });
+          ( [ "one.fs"; "two.fs"; "--project"; "../NProj.fsproj" ], { SourceFiles = [ "/home/vagrant/Working/NProj/one.fs" |> uri |> Compile; "/home/vagrant/Working/NProj/two.fs" |> uri |> Compile ];
+                                                                      ProjectFile = "/home/vagrant/Working/NProj" |> uri |> Directory }); ]
 
     [<Theory>]
     [<MemberData("parseAdd")>]
-    let ``Parse add arguments expect success`` (args: string seq) (expected: Add.AddCommand) = failwith "undefined"
+    let ``Parse add arguments expect success`` (args: string seq) (expected: Add.AddCommand) =
+        // Fixture setup
+        // Exercise system
+        let actual = Add.parse args
+        // Verify outcome
+        Assert.Equal(expected, actual)
