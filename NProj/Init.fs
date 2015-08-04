8bf2ec8a2826e6cff0a64cf889d61d5c3d1038df
Chris Bowdon <c.bowdon@bath.edu>
2015-08-04 21:24:17 +0000

    Drop in project init code from prototype

--- NProj/Init.fs
+++ NProj/Init.fs
@@ -2,6 +2,8 @@ namespace NProj
 
 module Init =
 
+    open System
+    open Microsoft.Build.Evaluation
     open Common
     open IO
 
@@ -49,4 +51,47 @@ module Init =
     let parse (args: string seq): FreeDisk<InitCommand> =
         foldParsers [ parseProjectFile; parseLang; parseType ] defaultInit args
 
-    let execute (cmd: InitCommand): unit = failwith "undefined"
+    let name (project: ProjectFileLocation): string =
+        match project with
+        | Directory x -> x.AbsolutePath |> System.IO.Path.GetDirectoryName
+        | File x -> x.AbsolutePath |> System.IO.Path.GetFileNameWithoutExtension
+
+    let execute (cmd: InitCommand): unit =
+        let name = name cmd.ProjectFile
+        let proj = Project()
+        proj.Xml.DefaultTargets <- "Build"
+        // Imports
+        let commonPropImport = proj.Xml.AddImport("$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props")
+        commonPropImport.Condition <- "Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')"
+        // Properties
+        proj.SetProperty("Language", sprintf "%A" cmd.Lang) |> ignore
+        proj.SetProperty("OutputType", sprintf "%A" cmd.Type) |> ignore
+        proj.SetProperty("Name", name) |> ignore
+        proj.SetProperty("RootNamespace", name) |> ignore
+        proj.SetProperty("AssemblyName", name) |> ignore
+        proj.SetProperty("SchemaVersion", "2.0") |> ignore
+        proj.SetProperty("ProjectGuid", Guid.NewGuid() |> sprintf "{%A}") |> ignore
+        // Default references
+        proj.AddItem("Reference", "mscorlib") |> ignore
+        proj.AddItem("Reference", "System") |> ignore
+        proj.AddItem("Reference", "System.Core") |> ignore
+        proj.AddItem("Reference", "System.Numerics") |> ignore
+        if cmd.Lang = FSharp
+        then
+            // Properties
+            proj.SetProperty("TargetFrameworkVersion", "v4.0") |> ignore
+            proj.SetProperty("TargetFSharpCoreVersion", "4.3.0.0") |> ignore
+            // References
+            proj.AddItem("Reference", "FSharp.Core, Version=$(TargetFSharpCoreVersion), Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a") |> ignore
+            // Targets
+            let pg = proj.Xml.AddPropertyGroup()
+            pg.Condition <- @"Exists('$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.0\Framework\v4.0\Microsoft.FSharp.Targets')"
+            pg.AddProperty("FSharpTargetsPath", @"$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.0\Framework\v4.0\Microsoft.FSharp.Targets") |> ignore
+            let fsTargetsImport = proj.Xml.AddImport("$(FSharpTargetsPath)")
+            fsTargetsImport.Condition <- "Exists('$(FSharpTargetsPath)')"
+        else ()
+        let saveLocation =
+            match cmd.ProjectFile with
+            | File x -> x
+            | Directory x -> Uri(x, cmd.Lang.Extension |> sprintf "%s.%s" name)
+        proj.Save(saveLocation.AbsolutePath)
